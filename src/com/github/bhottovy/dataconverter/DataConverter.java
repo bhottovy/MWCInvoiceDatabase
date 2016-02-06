569ab448ccc083c01c4de2d5614487cbd2c18fb9
nloos2 <Shadovore@gmail.com>
2016-02-06 15:24:55 -0600

    Delimeters

--- src/com/github/bhottovy/dataconverter/DataConverter.java
+++ src/com/github/bhottovy/dataconverter/DataConverter.java
@@ -1,4 +1,5 @@
 package com.github.bhottovy.dataconverter;
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 import com.github.bhottovy.dataconverter.person.Customer;
@@ -11,7 +12,7 @@ public class DataConverter {
 	public static final String CUSTOMER_FILE = "data/Customers.dat";
 	public static final String PRODUCT_FILE = "data/Products.dat";
 	
-	public static void main (String args[]) {
+	public static void main (String args[]) throws FileNotFoundException {
 		
 		Map<Person, String> persons = FileReader.importPersons(PERSON_FILE);
 		Map<Customer, String> customers = FileReader.importCustomers(CUSTOMER_FILE);
--- src/com/github/bhottovy/dataconverter/FileReader.java
+++ src/com/github/bhottovy/dataconverter/FileReader.java
@@ -1,6 +1,7 @@
 package com.github.bhottovy.dataconverter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,17 +12,24 @@ import com.github.bhottovy.dataconverter.product.Product;
 
 public class FileReader {
 
-	public static Map<Person, String> importPersons(String fileName) {
+	public static Map<Person, String> importPersons(String fileName) throws FileNotFoundException {
 		
 		Map<Person, String> map = new HashMap<Person, String>();
 		
 		File file = new File(fileName);
 		Scanner input = new Scanner(file);
 		
+		int count = input.nextInt();
+		input.useDelimiter(";");
+		input.nextLine();
+		
+		System.out.print(input.next());
+		System.out.print(input.next());
+		
 		return map;
 	}
 	
-	public static Map<Customer, String> importCustomers(String fileName) {
+	public static Map<Customer, String> importCustomers(String fileName) throws FileNotFoundException {
 		
 		Map<Customer, String> map = new HashMap<Customer, String>();
 		
@@ -31,7 +39,7 @@ public class FileReader {
 		return map;
 	}
 
-	public static Map<Product, String> importProducts(String fileName) {
+	public static Map<Product, String> importProducts(String fileName) throws FileNotFoundException {
 	
 		Map<Product, String> map = new HashMap<Product, String>();
 		
