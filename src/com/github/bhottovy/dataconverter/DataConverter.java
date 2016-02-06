efd42167c7168fbafc44e8330854a6db33a8265a
bhottovy <bhottovy@hotmail.com>
2016-02-06 15:10:58 -0600

    3

--- src/com/github/bhottovy/dataconverter/DataConverter.java
+++ src/com/github/bhottovy/dataconverter/DataConverter.java
@@ -1,6 +1,4 @@
 package com.github.bhottovy.dataconverter;
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import com.github.bhottovy.dataconverter.person.Customer;
@@ -8,20 +6,17 @@ import com.github.bhottovy.dataconverter.person.Person;
 import com.github.bhottovy.dataconverter.product.Product;
 
 public class DataConverter {
-	
-	Map<Person, String> persons = new HashMap<Person, String>();
-	Map<Customer, String> customers = new HashMap<Customer, String>();
-	Map<Product, String> products = new HashMap<Product, String>();
-	
+
 	public static final String PERSON_FILE = "data/Persons.dat";
 	public static final String CUSTOMER_FILE = "data/Customers.dat";
 	public static final String PRODUCT_FILE = "data/Products.dat";
 	
 	public static void main (String args[]) {
 		
-		this.persons = FileReader.importPersons(PERSON_FILE);
-		this.customers = FileReader.importCustomers(CUSTOMER_FILE);
-		this.products = FileReader.importProducts(PRODUCT_FILE);
-			   
+		Map<Person, String> persons = FileReader.importPersons(PERSON_FILE);
+		Map<Customer, String> customers = FileReader.importCustomers(CUSTOMER_FILE);
+		Map<Product, String> products = FileReader.importProducts(PRODUCT_FILE);
+		
+		//FileWriter.exportData(persons, customers, products);
 	}
 }
