dcf01a17eb22608cea34d94a514cdfcf26ef26dd
nloos2 <Shadovore@gmail.com>
2016-02-06 14:34:07 -0600

    Committed DataConverter

--- src/com/github/bhottovy/dataconverter/DataConverter.java
+++ src/com/github/bhottovy/dataconverter/DataConverter.java
@@ -1,8 +1,28 @@
 package com.github.bhottovy.dataconverter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import Persons;
 
 public class DataConverter {
-	
-	public static void main(String[] args) {
+	public static void main (String args[]) throws FileNotFoundException{
 		
+		File file1 = new File("data/Customers.dat");
+		File file2 = new File("data/Persons.dat");
+		File file3 = new File("data/Products.dat");
+		Scanner FS = new Scanner(file1);
+		Scanner FS2 = new Scanner(file2);
+		Scanner FS3 = new Scanner(file3);
+		
+		Map<Integer, Persons> map = new HashMap<Integer, Persons>();
+		
+		for (int i=0; i<map.size(); ++i){
+			map.put(Persons., value)
+		}
+
+			   
 	}
 }
