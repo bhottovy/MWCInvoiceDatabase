01652eb86672d274ec7e76500595db244981f68e
bhottovy <bhottovy@hotmail.com>
2016-02-06 14:05:28 -0600

    New Repository

---
+++ src/com/github/bhottovy/dataconverter/information/Address.java
@@ -0,0 +1,5 @@
+package com.github.bhottovy.dataconverter.information;
+
+public class Address {
+
+}
---
+++ src/com/github/bhottovy/dataconverter/person/Person.java
@@ -0,0 +1,5 @@
+package com.github.bhottovy.dataconverter.person;
+
+public class Person {
+
+}
