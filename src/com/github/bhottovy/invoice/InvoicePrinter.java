9b22f22580bf0b1d25572ad02344b205a7db4af7
bhottovy <bhottovy@hotmail.com>
2016-02-21 16:00:50 -0600

    4:00 pm

--- src/com/github/bhottovy/invoice/InvoicePrinter.java
+++ src/com/github/bhottovy/invoice/InvoicePrinter.java
@@ -1,12 +1,10 @@
 package com.github.bhottovy.invoice;
 
-import java.util.ArrayList;
-
-import com.github.bhottovy.dataconverter.information.Invoice;
+import com.github.bhottovy.dataconverter.information.Invoices;
 
 public class InvoicePrinter {
 
-	public static void printReport(ArrayList<Invoice> invoices) {
+	public static void printReport(Invoices invoices) {
 		
 	}
 }
